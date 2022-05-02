c00adb512a9936692faec9b7319639241920f429
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2022-05-02 16:31:45 -0700

    change to port

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ const {
 } = require('express/lib/response');
 
 const app = express();
+
+app.set('port', process.env.PORT || 3000)
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
     res.status(200).send("OK");
 })
 
-app.listen(3000, () => {
+app.listen(app.get('port'), () => {
     console.log("Corriendo en el puerto 3000")
 })
